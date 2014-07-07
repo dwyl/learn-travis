cd35814b5c04004353a9b5c8cd2746daf36c2515
nelsonic <nodecoder@gmail.com>
2014-07-07 11:46:37 +0100

    re-instates grunt.js

---
+++ grunt.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    lint: {
+      files: ['hello.js']
+    }
+  });
+
+  grunt.registerTask('default', 'lint');
+  grunt.registerTask('travis', 'lint');
+  
+};
